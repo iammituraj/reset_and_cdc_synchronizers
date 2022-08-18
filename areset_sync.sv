cc680fe229a0a870f9afaebbfd94247d38f21102
Mitu <iammituraj@gmail.com>
2022-08-18 21:06:14 +0530

    Renamed file

--- areset_sync.sv
+++ areset_sync.sv
@@ -1,7 +1,7 @@
 /*===============================================================================================================================
-   Module       : Reset Synchronizer
+   Module       : Async Reset Synchronizer
 
-   Description  : Reset Synchronizer synchronizes the de-assertion of asynchronous reset to the design's clock domain.               
+   Description  : Async Reset Synchronizer synchronizes the de-assertion of asynchronous reset to the design's clock domain.               
 
    Developer    : Mitu Raj, chip@chipmunklogic.com at Chipmunk Logic ™, https://chipmunklogic.com
    Notes        : Attribute ASYNC_REG used to PAR the flops together in Xilinx FPGAs.
@@ -10,10 +10,10 @@
 ===============================================================================================================================*/
 
 /*-------------------------------------------------------------------------------------------------------------------------------
-                                                R E S E T   S Y N C H R O N I Z E R
+                                          A S Y N C   R E S E T   S Y N C H R O N I Z E R
 -------------------------------------------------------------------------------------------------------------------------------*/
 
-module reset_sync #(
+module areset_sync #(
    
    // Configurable parameters   
    parameter STAGES  = 2    ,        // No. of flops in the chain, min. 2
@@ -49,5 +49,5 @@ assign o_rst_sync = sync_ff [STAGES-1] ;
 endmodule
 
 /*-------------------------------------------------------------------------------------------------------------------------------
-                                                R E S E T   S Y N C H R O N I Z E R
+                                          A S Y N C   R E S E T   S Y N C H R O N I Z E R
 -------------------------------------------------------------------------------------------------------------------------------*/
