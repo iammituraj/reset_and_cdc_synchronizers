77067f97629651717fbe2fc178ada19c5b630020
Mitu <iammituraj@gmail.com>
2022-08-27 01:01:15 +0530

    Added new cdc syncs

--- cdc_sync_with_clr.sv
+++ cdc_sync_with_clr.sv
@@ -13,14 +13,15 @@
                                                    C D C   S Y N C H R O N I Z E R
 -------------------------------------------------------------------------------------------------------------------------------*/
 
-module cdc_sync #(
+module cdc_sync_with_clr #(
    
    // Configurable parameters   
    parameter STAGES = 2             // No. of flops in the chain, min. 2
 )
 
 (
-   input  logic clk        ,        // Clock @ destination clock domain   
+   input  logic clk        ,        // Clock @ destination clock domain
+   input  logic rstn       ,        // Async Reset @ destination clock domain
    input  logic i_sig      ,        // Input signal, asynchronous
    output logic o_sig_sync          // Output signal synchronized to clk
 ) ;
@@ -29,8 +30,15 @@ module cdc_sync #(
 logic [STAGES-1: 0] sync_ff ;
 
 // Synchronizing logic
-always @(posedge clk) begin   
-   sync_ff <= {sync_ff [STAGES-2 : 0], i_sig} ;   
+always @(posedge clk or negedge rstn) begin
+   
+   if (!rstn) begin
+      sync_ff <= '0 ;
+   end
+   else begin
+      sync_ff <= {sync_ff [STAGES-2 : 0], i_sig} ;     
+   end  
+
 end
 
 // Synchronized signal
